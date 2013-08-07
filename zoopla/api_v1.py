2cda6c76133a7da601b0c6c860ea7effb8b983b5
Paul Furley <paul@paulfurley.com>
2013-08-07 15:21:33 +0100

    Implemented the property listings API call. Needs some generalising across other methods

--- zoopla/api_v1.py
+++ zoopla/api_v1.py
@@ -4,11 +4,31 @@
 import requests
 import requests_cache
 from cStringIO import StringIO
+import json
+from urllib import urlencode
+import logging
+L = logging.getLogger(__name__)
+
+BASE_URL = 'http://api.zoopla.co.uk/api/v1/'
+
 
 class _ApiVersion1(object):
-    def __init__(self, api_key, session_id=None):
-        self.api_key = api_key
-        print("{}, {}".format(api_key, session_id))
+    def __init__(self, api_key, session_id=None, cache_seconds=(12 * 60 * 60)):
+        self.api_key = self._validate_api_key(api_key)
+        if cache_seconds:
+            install_cache(cache_seconds)
+
+    def _validate_api_key(self, api_key):
+        if len(api_key) < 24:
+            raise ValueError("Invalid API key(?): '{}'".format(api_key))
+        return api_key
+
+    def _make_url(self, command, arguments):
+        arguments['api_key'] = self.api_key
+        url = "{}{}.js?{}".format(BASE_URL, command,
+                                  urlencode(sort_dict(arguments)))
+        L.debug(url)
+        return url
 
     def zed_index(self):
         raise NotImplementedError("This method isn't yet implemented.")
@@ -32,7 +52,12 @@ class _ApiVersion1(object):
         raise NotImplementedError("This method isn't yet implemented.")
 
     def property_listings(self, **kwargs):
+        L.info('property_listings: {}'.format(kwargs))
         validate_query_arguments(kwargs)
+        url = self._make_url('property_listings', kwargs)
+        f = download_url(url)
+        result = json.loads(f.read())
+        L.debug(result)
         raise NotImplementedError("This method isn't yet implemented.")
 
     def get_session_id(self):
@@ -52,6 +77,7 @@ class _ApiVersion1(object):
 
 
 def install_cache(expire_after):
+    L.info("Installing cache, valid for {} seconds.".format(expire_after))
     requests_cache.install_cache(
         expire_after=expire_after,
         allowable_methods=('GET',))
@@ -63,6 +89,16 @@ def download_url(url):
     return StringIO(response.content)
 
 
+def sort_dict(some_dict):
+    """
+
+    Return a unicode:unicode dictionary, sorted by the key.
+    >>> sort_dict({'b': 1, 'a': 2})
+    [('a', 2), ('b', 1)]
+    """
+    return sorted(some_dict.items())
+
+
 def validate_query_arguments(arguments):
     validated = {}
     for argument, value in arguments.items():
@@ -70,11 +106,15 @@ def validate_query_arguments(arguments):
 
 
 def validate_argument(name, value):
+    validate_func_name = 'validate_' + name
     try:
-        validate_func = globals()['validate_' + name]
+        validate_func = globals()[validate_func_name]
     except KeyError:
+        L.debug("No function {}(..), returning '{}' as '{}'".format(
+            validate_func_name, name, value))
         return value
     else:
+        L.debug("Calling {}({})".format(validate_func_name, value))
         return validate_func(value)
 
 
